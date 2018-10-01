28ec949f3b516014397e59412de59b8a684c2f13
Denis Biondic <denis@coffeeapplied.com>
2018-10-01 11:09:43 +0200

    feat: show notification in frontend

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HubConnectionBuilder } from '@aspnet/signalr';
+import { MessageService } from 'primeng/api';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [MessageService],
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+  constructor(private messageService: MessageService) { }
+
+  ngOnInit(): void {
+    let connection = new HubConnectionBuilder()
+      .withUrl(environment.backendUrl + "/notificationhub")
+      .build();
+
+    connection.on("NewNotification", (msg) => {
+      console.log(`Notification recieved ${msg}`);
+      this.messageService.add({ severity: 'warn', summary: 'Notification', detail: msg });
+    });
+
+    connection.start();
+  }
 }
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
+import { ToastModule } from 'primeng/toast';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +10,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ToastModule
   ],
   providers: [],
   bootstrap: [AppComponent]
